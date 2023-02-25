4e31dd74f9e559f43bf09900a03a29173891d2b9
Kamil Musial <kamil.musial@tietoevry.com>
2023-02-25 11:57:16 +0100

    selenium2_v2

--- selenium2.py
+++ selenium2.py
@@ -1,22 +1,32 @@
 from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
 import time
+import datetime
+
+def make_screenshot(driver):
+    teraz = datetime.datetime.now()
+    screenshot = 'screenshot' + teraz.strftime('_%H%M%S') + '.png'
+    driver.get_screenshot_as_file(screenshot)
 
 driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
 print('Nazwa strony',driver.title)
 time.sleep(1)
 
-username_field = driver.find_element('id', 'user-name')
-username_field.clear()
-username_field.send_keys('standard_user')
+try:
+    username_field = driver.find_element('id', 'user-name')
+    username_field.clear()
+    username_field.send_keys('standard_user')
+except:
+    make_screenshot(driver)
 
-password_field = driver.find_element('id', 'password')
+password_field = driver.find_element('xpath', '//*[@id="password"]')
 username_field.clear()
 password_field.send_keys('secret_sauce')
 
 login_button = driver.find_element('name', 'login-button')
-login_button.click()
+if not login_button.get_attribute('disabled'):
+        login_button.click()
 
 time.sleep(1)
 driver.get_screenshot_as_file('screenshot.png')
