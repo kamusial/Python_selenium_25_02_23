52aee1c08dc95a1ae3f93a79ea87c259cda0d654
Kamil Musial <kamil.musial@tietoevry.com>
2023-02-26 09:47:40 +0100

    wszystko z selenium

--- selenium2.py
+++ selenium2.py
@@ -5,32 +5,34 @@ import time
 import datetime
 from selenium.common.exceptions import NoSuchElementException
 
+
 def make_screenshot(driver):
     teraz = datetime.datetime.now()
     screenshot = 'screenshot' + teraz.strftime('_%H%M%S') + '.png'
     driver.get_screenshot_as_file(screenshot)
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
-print('Nazwa strony',driver.title)
-time.sleep(1)
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
+    print('Nazwa strony',driver.title)
+    time.sleep(1)
 
-try:
-    username_field = driver.find_element(By.ID, 'user-name')
-except NoSuchElementException:
-    make_screenshot(driver)
-    raise
+    try:
+        username_field = driver.find_element(By.ID, 'user-name')
+    except NoSuchElementException:
+        make_screenshot(driver)
+        raise
 
-username_field.clear()
-username_field.send_keys('standard_user')
-password_field = driver.find_element('xpath', '//*[@id="password"]')
-username_field.clear()
-password_field.send_keys('secret_sauce')
+    username_field.clear()
+    username_field.send_keys('standard_user')
+    password_field = driver.find_element('xpath', '//*[@id="password"]')
+    username_field.clear()
+    password_field.send_keys('secret_sauce')
 
-login_button = driver.find_element('name', 'login-button')
-if not login_button.get_attribute('disabled'):
-        login_button.click()
+    login_button = driver.find_element('name', 'login-button')
+    if not login_button.get_attribute('disabled'):
+            login_button.click()
 
-time.sleep(1)
-driver.get_screenshot_as_file('screenshot.png')
-driver.quit()
+    time.sleep(1)
+    driver.get_screenshot_as_file('screenshot.png')
+    driver.quit()
--- selenium5.py
+++ selenium5.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium4 import LoginPage
 import time
+from selenium2 import make_screenshot
+import pytest
 
 def test_login_page():
     driver = webdriver.Chrome()
@@ -10,5 +12,14 @@ def test_login_page():
     page.enter_password('secret_sauce')
     page.click_login()
     time.sleep(1)
-    assert driver.current_url == 'https://www.saucedemo.com/inventory.html'
-    driver.quit()
+    try:
+        assert driver.current_url == 'https://www.saucedemo.com/inventory.html', make_screenshot(driver)
+    except AssertionError:
+        print('Assercja nie przeszła')
+        raise
+    else:
+        print('assercja przeszła')
+    finally:
+        print('po asercji')
+        driver.quit()
+#
